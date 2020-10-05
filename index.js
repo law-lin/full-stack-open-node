ff83184e4f78d25540af4ba603096fb426a20712
law-lin <lawrencelin101@gmail.com>
2020-10-05 13:27:16 -0400

    react build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
-
+app.use(express.static('build'));
 app.use(cors());
 const PORT = process.env.PORT || 3001;
 console.log(`Server running on port ${PORT}`);
