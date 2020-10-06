0a3153bde4846267a412b49ea29e226cb870069a
law-lin <lawrencelin101@gmail.com>
2020-10-06 17:10:52 -0400

    react build

---
+++ build/precache-manifest.e4b4037bf4cbe4761c625fce7076a3ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354ae1485822930851c8c63218db99e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b18700bf85722ae74b1",
+    "url": "/static/css/main.1fcd24fd.chunk.css"
+  },
+  {
+    "revision": "2469ec848eef6bb55628",
+    "url": "/static/js/2.90705a08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90705a08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b18700bf85722ae74b1",
+    "url": "/static/js/main.7f0b626e.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
